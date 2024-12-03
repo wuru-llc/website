fe67f811a3b4cb8da21966e42d1b112800c11b62
Diego Gonzalez King <dgkbsasl@gmail.com>
2024-12-03 13:38:33 -0300

    remove server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 4321,
-  },
-});
+export default defineConfig({});
