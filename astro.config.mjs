8ad6b8c739ea2ce93a1502b08be18b098f724199
Javi Roberts <jrobertsotero@gmail.com>
2024-12-26 17:39:44 -0300

    updating website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wuru-llc.github.io",
-    base: "website",
+    site: "https://www.wuru.ai",
+    base: "",
 });
