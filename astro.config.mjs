3988709d7ed5d2fd8bd74ad3e36d9d2587db44e6
Diego Gonzalez King <dgkbsasl@gmail.com>
2025-01-09 10:00:32 -0300

    change molina to molinos. change Linkedin link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.wuru.ai",
-    base: "",
+  site: "https://www.wuru.ai",
+  base: "",
+  server: {
+    host: true,
+    port: 4321,
+  },
 });
