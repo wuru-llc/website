c968d9be0bd66c6a5cc3caffe50f06e5460de263
Diego Gonzalez King <dgkbsas@gmail.com>
2025-06-12 18:07:51 -0300

    base init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
+// import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.wuru.ai",
   base: "",
   output: "server",
-  adapter: vercel()
+  // adapter: vercel(),
 });
